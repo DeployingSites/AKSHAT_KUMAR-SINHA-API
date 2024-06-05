92c18adef58cf2f235bfb5634e7b534e86eb84f9
DeployingSites <kumarsinhaakshat19@gmail.com>
2024-06-05 13:48:32 +0530

    commit

--- server.js
+++ server.js
@@ -5,16 +5,17 @@ const cors = require('cors')
 const path = require('path')
 const server = express();
 
-
 server.use(cors());
 server.use(express.json());
 server.use('/api/music-player',music_router)
 server.use('/api/music-player/resource/music', express.static(path.join(__dirname, 'resource/music')));
 server.use('/api/music-player/resource/img', express.static(path.join(__dirname, 'resource/img')));
+server.use('/dashboard', express.static(path.join(__dirname, '/dashboard')));
+
 
 server.get('/',async (req,res)=>{
     try{
-        res.send("Welcome to Akshat kumar Sinha's Api")
+        res.sendFile(path.join(__dirname,'/index.html'))
     }catch(error){
         console.log(error)
     }
